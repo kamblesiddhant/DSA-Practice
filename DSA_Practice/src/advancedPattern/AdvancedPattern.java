2eb2f8c5ce637452706bf4567a7c224b8e608004
kamblesiddhant <kamblesiddhant081@gmail.com>
2026-07-24 14:23:46 +0530

    inversion roteted half pyramid pattern

--- DSA_Practice/src/advancedPattern/AdvancedPattern.java
+++ DSA_Practice/src/advancedPattern/AdvancedPattern.java
@@ -16,7 +16,20 @@ public class AdvancedPattern {
         }
     }
 
+    public static void interted_roteted_half_pyramid(int range){
+        for(int i=1; i<=range; i++){
+            for (int j=1; j<=(range-i); j++){
+                System.out.print(" ");
+            }
+            for (int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        hollowRectangle(5);
+//        hollowRectangle(5);
+        interted_roteted_half_pyramid(6);
     }
 }
