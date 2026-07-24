c82a8d11254ca50aae37a91c7b583a18f25bef84
kamblesiddhant <kamblesiddhant081@gmail.com>
2026-07-24 14:47:51 +0530

    trangle binary pattern

--- DSA_Practice/src/advancedPattern/AdvancedPattern.java
+++ DSA_Practice/src/advancedPattern/AdvancedPattern.java
@@ -48,10 +48,25 @@ public class AdvancedPattern {
         }
     }
 
+    public static void trangleBin(int range){
+        for (int i=1; i<=range;i++){
+            for (int j=1; j<=i;j++){
+                if ((i+j)%2 ==0){
+                    System.out.print("1");
+                }else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 //        hollowRectangle(5);
 //        interted_roteted_half_pyramid(6);
 //        inverted_half_pattern_number(5);
-        floydTrangle(7);
+//        floydTrangle(7);
+        trangleBin(5);
+
     }
 }
