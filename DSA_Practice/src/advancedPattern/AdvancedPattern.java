c23eb86c91661d237494b6f7061dc56afe2171b8
kamblesiddhant <kamblesiddhant081@gmail.com>
2026-07-24 14:57:17 +0530

    butterfly pattern

--- DSA_Practice/src/advancedPattern/AdvancedPattern.java
+++ DSA_Practice/src/advancedPattern/AdvancedPattern.java
@@ -61,12 +61,44 @@ public class AdvancedPattern {
         }
     }
 
+    public static void butterflyPattern(int n){
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            for (int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i=n; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            for (int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 //        hollowRectangle(5);
 //        interted_roteted_half_pyramid(6);
 //        inverted_half_pattern_number(5);
 //        floydTrangle(7);
-        trangleBin(5);
-
+//        trangleBin(5);
+        butterflyPattern(5);
     }
 }
