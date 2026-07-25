9b3bb3589812c45131a004dcbb157c9ea040422b
kamblesiddhant <kamblesiddhant081@gmail.com>
2026-07-25 23:38:11 +0530

    create array

---
+++ DSA_Practice/src/arrays/ArrayBasics.java
@@ -0,0 +1,21 @@
+package arrays;
+
+// in that class cover all basics of array
+// create, input, Output, Update
+public class ArrayBasics {
+
+
+
+    public static void main(String[] args) {
+
+        // Syntax
+        // datatype arrayName[] = new dataType[size]
+
+        // create array
+        int arr[] = new int[10]; // array is static type ones you declare a size you can't able to change
+
+        int number[] = {1,2,3,4};
+
+        String[] fruits = {"mango", "orange", "apple"};
+    }
+}
