132d4d420e3c7d488205c9356342f063a66b52cd
kamblesiddhant <kamblesiddhant081@gmail.com>
2026-07-27 15:55:29 +0530

    binary search

---
+++ DSA_Practice/src/arrays/BinarySearch.java
@@ -0,0 +1,31 @@
+package arrays;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] num, int key){
+        int mid = 0;
+        int start = 0;
+        int end = (num.length-1);
+
+        for(int i=0; i<num.length; i++){
+
+            mid = (start+end)/2;
+           if (num[mid] == key){
+               return mid;
+           } else if (key > num[mid]) {
+               start = mid + 1;
+           }else {
+               end = mid - 1;
+           }
+        }
+
+        return mid;
+    }
+
+    public static void main(String[] args) {
+
+        int[] num = {2,3,4,5,7,9};
+        int ans = binarySearch(num, 9);
+        System.out.println("the key is in "+ans+" index");
+    }
+}
