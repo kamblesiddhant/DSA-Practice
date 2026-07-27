8755ef035434a5b0fdd32714d0328d59e7a69d1d
kamblesiddhant <kamblesiddhant081@gmail.com>
2026-07-27 16:57:13 +0530

    print subarray

---
+++ DSA_Practice/src/arrays/PringSubArray.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class PringSubArray {
+
+    public static void printSubArray(int arr[]){
+        for (int i=0; i<arr.length;i++){
+            for (int j=i; j<arr.length;j++){
+                for (int k = i; k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        printSubArray(arr);
+    }
+}
